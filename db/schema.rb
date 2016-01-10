1a42818ff7106f3ac0c92dafe09e9e5f954c3cf8
Sky Su <februaryskys@gmail.com>
2016-01-10 14:15:39 +0000

    Adds cancancan gem, adds role to Users, and generates cancan:ability

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110114330) do
+ActiveRecord::Schema.define(version: 20160110141413) do
 
   create_table "ttt_games", force: :cascade do |t|
     t.string   "player1",        default: "---\n:id: \n:symbol: \n"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160110114330) do
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
