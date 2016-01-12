67fca6fbbd9e31674a26976fb02dda68941104ba
Sky Su <februaryskys@gmail.com>
2016-01-12 13:35:41 +0000

    Customizes User registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :ttt_games
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'ttt_games#index'
   # The priority is based upon order of creation: first created -> highest priority.
